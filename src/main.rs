e5157f163745a5b12aced348665ed5139ecca2d7
arandombit <a.random.luis@proton.me>
2025-11-04 23:32:33 -0500

    define struct for grids

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ struct Cell {
   alive: bool
 }
 
+struct Grid {
+  height: usize,
+  width: usize,
+  cells: Vec<Vec<Cell>>
+}
+
 fn main() {
   let mut grid = Vec::new();
   println!("Hello, world!");
