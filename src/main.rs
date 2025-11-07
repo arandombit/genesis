6d77983b3ce9094d6b18a7f63c46f60ca3c8ec68
arandombit <a.random.luis@proton.me>
2025-11-06 22:02:30 -0500

    create constructor for initializing a grid of cells

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,13 @@ struct Grid {
   cells: Vec<Vec<Cell>>
 }
 
+impl Grid {
+  fn new(height: usize, width: usize) -> Self {
+    let cells = vec![vec![Cell { alive: false }; height]; width];
+    Grid { height, width, cells }
+  }
+}
+
 fn main() {
   println!("Initiating genesis...");
   let mut grid = Grid { height: 1024, width: 1024, cells: Vec::new() }
